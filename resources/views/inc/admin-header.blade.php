befadb0f49f8b3683f3ecec60ba423e3a2cf9ac1
mykhailoiukysh <mykhailoiukysh@mykhailoiukysh.com>
2020-08-04 20:23:53 +0700

    favicon

--- resources/views/inc/admin-header.blade.php
+++ resources/views/inc/admin-header.blade.php
@@ -20,7 +20,7 @@
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="{{asset('assets/admin/css/custom.css')}}">
     <!-- Favicon-->
-    <link rel="shortcut icon" href="{{asset('assets/admin/img/favicon-48 x48.png')}}">
+    <link rel="shortcut icon" href="{{asset('assets/admin/img/favicon.ico')}}">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
